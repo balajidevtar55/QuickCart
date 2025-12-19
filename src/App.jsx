60f27dee63ea56c9145deab283b8810b70ae1d1d
balajidevtar55 <balajidevtarwork@gmail.com>
2025-12-19 20:57:35 +0530

    some changes

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,15 @@ import { Register } from './pages/user/register.jsx';
 import { Login } from './pages/user/login.jsx';
 import Navbar from './pages/navbar.jsx';
 import { CustomerDashboard } from './pages/customer/CustomerDashboard.jsx';
+import { CheckerDashboard } from './pages/customer/CheckerDashboard.jsx';
+import { ProtectedRoute } from "./hoc/ProtectedRoute.jsx"
+import {Unauthorized} from "./pages/Unauthorized.jsx"
+import ProductReport from './pages/owner/ProductReport';
+import SalesReport from './pages/owner/SalesReport';
+import Inventory from './pages/manager/Inventory';
+import OrdersToday from './pages/manager/OrdersToday';
+import History from './pages/checker/History.jsx';
+import CustomerReport from './pages/customer/customerReport.jsx';
 function App() {
 
   return (
@@ -15,11 +24,25 @@ function App() {
          <Navbar />
 
       <Routes>
-        <Route path="/" element={<ProductList />} />
-        <Route path="/addproduct" element={<ProductAdd />} />
-        <Route path="/register" element={<Register />} />
+        <Route path="/admin-dashboard"  element={<ProtectedRoute roles={["admin"]}><ProductList /></ProtectedRoute> } />
+        <Route path="/" element={<ProtectedRoute ><CustomerDashboard /></ProtectedRoute> } />
+        <Route path="/addproduct" element={<ProtectedRoute roles={["admin"]} > <ProductAdd /> </ProtectedRoute >} />
+        <Route path="/checker-dashboard"  element={<ProtectedRoute roles={["checker"]}> <CheckerDashboard /> </ProtectedRoute >} />
+        <Route path="/register"  element={<Register />} />
+        <Route path="/productReport"  element={<ProductReport />} />
+        <Route path="/salesReport"  element={<SalesReport />} />
+        <Route path="/inventory"  element={<Inventory />} />
+        <Route path="/orderstoday"  element={<OrdersToday />} />
+
+       <Route path="/checker-history" element={<History />} />
+       <Route path="/customer-report" element={<CustomerReport />} />
+
+   
+   
         <Route path="/login" element={<Login />} />
-        <Route path="/customer-dashboard" element={<CustomerDashboard />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/customer-dashboard"  element={<ProtectedRoute roles={["customer"]}><CustomerDashboard /></ProtectedRoute >} />
+        <Route path="*" element={<Unauthorized />} />
 
       
 
